425010e1c05b62449b2ab1dc153608c97f0f9993
707sss <github707sss@gmail.com>
2026-03-14 13:08:09 +0300

    Rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+// Oy Fevral
+$Fevral19 = "1 Pa Fevral-19 Saharlik 06:06 || Iftorlik 18:23";
+$Fevral20 = "2 Ju Fevral-20 Saharlik 06:05 || Iftorlik 18:22";
+$Fevral21 = "3 SHa Fevral-21 Saharlik 06:04 || Iftorlik 18:24";
+$Fevral22 = "4 Ya Fevral-22 Saharlik 06:02 || Iftorlik 18:25";
+$Fevral23 = "5 Du Fevral-23 Saharlik 06:01 || Iftorlik 18:26";
+$Fevral24 = "6 Se Fevral-24 Saharlik 05:59 || Iftorlik 18:27";
+$Fevral25 = "7 CHo Fevral-25 Saharlik 05:58 || Iftorlik 18:28";
+$Fevral26 = "8 Pa Fevral-26 Saharlik 05:57 || Iftorlik 18:30";
+$Fevral27 = "9 JU Fevral-27 Saharlik 05:55 || Iftorlik 18:31";
+$Fevral28 = "10 SHa Fevral-28 Saharlik 05:55 || Iftorlik 18:32";
+// Oy mart 
+$Mart1 = "11 Ya Mart-1 Saharlik 05:53 || Iftorlik 18:33";   
+$Mart2 = "12 Du Mart-2 Saharlik 05:52 || Iftorlik 18:34";
+$Mart3 = "13 Sa Mart-3 Saharlik 05:50 || Iftorlik 18:35";
+$Mart4 = "14 CHo Mart-4 Saharlik 05:48 || Iftorlik 18:37";
+$Mart5 = "15 Pa Mart-5 Saharlik 05:47 || Iftorlik 18:38";
+$Mart6 = "16 Ju Mart-6 Saharlik 05:45 || Iftorlik 18:39";
+$Mart7 = "17 SHa Mart-7 Saharlik 05:44 || Iftorlik 18:40";
+$Mart8 = "18 Ya Mart-8 Saharlik 05:42 || Iftorlik 18:41";
+$Mart9 = "19 Du Mart-9 Saharlik 05:40 || Iftorlik 18:42";
+$Mart10 = "20 Se Mart-10 Saharlik 05:40 || Iftorlik 18:43";
+$Mart11 = "21 CHo Mart-11 Saharlik 05:38 || Iftorlik 18:42";
+$Mart12 = "22 Pa Mart-12 Saharlik 05:36 || Iftorlik 18:44";
+$Mart13 = "23 Ju Mart-13 Saharlik 05:35 || Iftorlik 18:45";
+$Mart14 = "24 SHa Mart-14 Saharlik 05:33 || Iftorlik 18:46";
+$Mart15 = "25 Ya Mart-15 Saharlik 05:31 || Iftorlik 18:47";
+$Mart16 = "26 Du Mart-16 Saharlik 05:29 || Iftorlik 18:48";
+$Mart17 = "27 Se Mart-17 Saharlik 05:28 || Iftorlik 18:49";
+$Mart18 = "28 CHo Mart-18 Saharlik 05:26 || Iftorlik 18:50";
+$Mart19 = "29 Pa Mart-19 Saharlik 05:24 || Iftorlik 18:51";
+$Mart20 = "30 Ju Mart-20 Saharlik 05:23 || Iftorlik 18:53";
+
+
+
+if ($Mart14 != date("Y-m-d")) {
+    echo $Mart14;
+}
+
+?>
